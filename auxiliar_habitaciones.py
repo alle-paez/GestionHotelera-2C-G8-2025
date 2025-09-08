c0b543aa2b0da597f3166e2d73f19657e4bcf189
facvillalba <facvillalba@uade.edu.ar>
2025-09-08 20:18:34 -0300

    avance habitaciones

--- auxiliar_habitaciones.py
+++ auxiliar_habitaciones.py
@@ -6,28 +6,31 @@ estados = ["Disponible", "Ocupada", "Mantenimiento"]
 def leer_tipo():
     tipo = input("Escribí el tipo de habitación (Single/Doble/Triple/Suite): ").capitalize()
     valido = list(filter(lambda x: x == tipo, tipos))
-
     while len(valido) == 0:
         tipo = input("Tipo inválido. Volvé a escribir: ").capitalize()
         valido = list(filter(lambda x: x == tipo, tipos))
-
     return tipo
 
 def leer_estado():
     estado = input("Escribí el estado (Disponible/Ocupada/Mantenimiento): ").capitalize()
     valido = list(filter(lambda x: x == estado, estados))
-
     while len(valido) == 0:
         estado = input("Estado inválido. Volvé a escribir: ").capitalize()
         valido = list(filter(lambda x: x == estado, estados))
-
     return estado
 
 def ubicar(matriz, item):
+    flag = 0
     i = 0
-    while i < len(matriz) and matriz[i][0] != item:
+    pos = -1
+    while flag != 1:
+        if matriz[i][0] == item:
+            flag = 1
+            pos = i
         i += 1
-    return i if i < len(matriz) else -1
+        if i == len(matriz):
+            flag = 1
+    return pos
 
 def leer_numero(mensaje, permitir_menos1=False):
     """
